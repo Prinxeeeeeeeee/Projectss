3a618f2fd8dfe1b22514e578ceb78db5ee390cf4
Prinxeeeeeeeee <sahprince109@gmail.com>
2024-09-14 12:38:59 +0545

    created page

---
+++ projects1/p.js
@@ -0,0 +1,12 @@
+// script.js
+const products = document.querySelectorAll('.product');
+
+products.forEach((product) => {
+  product.addEventListener('mouseover', () => {
+    product.style.transform = 'scale(1.1)';
+  });
+
+  product.addEventListener('mouseout', () => {
+    product.style.transform = 'scale(1)';
+  });
+});
